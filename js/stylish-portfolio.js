200889d7806e29d4dadcc1ffd9a08cde9d24d0cc
Jungo Kim <kismi5jung@gmail.com>
2018-03-28 17:16:56 +0900

    Introduce T-MAP FINDER initial codes

---
+++ js/stylish-portfolio.js
@@ -0,0 +1,55 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+    // 페이지가 로딩이 된 후 호출하는 함수입니다.
+  function initTmap(){
+    // map 생성
+    // Tmap.map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
+    var map = new Tmap.Map({div:'tmap', // map을 표시해줄 div
+                width:'100%',  // map의 width 설정
+                height:'100%' // map의 height 설정
+    }); 
+  } 
+  // 맵 생성 실행
+  initTmap();
+
+})(jQuery); // End of use strict
